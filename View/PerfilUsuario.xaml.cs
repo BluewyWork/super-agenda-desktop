332218e0a3faf70415625043d04f096ff111a93f
BluewyWork <bluewy.s.work@gmail.com>
2024-09-21 21:45:37 +0100

    refactor

---
+++ View/PerfilUsuario.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SuperAgenda.Models;
+using SuperAgenda.ViewModels;
+using SuperAgenda.View;
+
+namespace SuperAgenda
+{
+    /// <summary>
+    /// Lógica de interacción para PerfilUsuario.xaml
+    /// </summary>
+    public partial class PerfilUsuario : Window
+    {
+        public PerfilUsuario()
+        {
+            InitializeComponent();
+        }
+
+        public PerfilUsuario(EmpleadoModel u)
+        {
+            PerfilUsuarioVM r = new PerfilUsuarioVM(this);
+            DataContext = r;
+            InitializeComponent();
+            r.emp = u;
+            VName.Text = u.Name;
+                }
+
+       
+        private void Atras_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ ViewModels/PerfilVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using wpfappintermodular.api;
+using SuperAgenda.Models;
+using SuperAgenda.rsc;
+
+namespace SuperAgenda.ViewModels
+{
+    class PerfilUsuarioVM : INotifyPropertyChanged
+    {
+        private ApiService apiService;
+        public EmpleadoModel emp;
+        private PerfilUsuario view;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PerfilUsuarioVM(PerfilUsuario view)
+        {
+            this.view = view;
+            apiService = new ApiService(); 
+            GuardarCommand = new RelayCommand(Guardar);
+        }
+
+        public ICommand GuardarCommand { get; }
+        public ICommand AtrasCommand { get; }
+
+         
+        private async void Guardar()
+        {
+            emp.Name = view.VName.Text;
+             await apiService.UpdateEmployee(emp);
+        }
+
+   
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
